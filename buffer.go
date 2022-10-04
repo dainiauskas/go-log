c1e0c9ce4cd9a434eda9445d05a078c3e94a851a
Dainius Matusevicius <dainiauskas@gmail.com>
2022-10-04 08:26:59 +0300

    Pašalinta nDigits nenaudojama funkcija

--- buffer.go
+++ buffer.go
@@ -23,20 +23,6 @@ func (buf *buffer) twoDigits(i, d int) {
 	buf.tmp[i] = digits[d%10]
 }
 
-// nDigits formats an n-digit integer at buf.tmp[i],
-// padding with pad on the left.
-// It assumes d >= 0.
-func (buf *buffer) nDigits(n, i, d int, pad byte) {
-	j := n - 1
-	for ; j >= 0 && d > 0; j-- {
-		buf.tmp[i+j] = digits[d%10]
-		d /= 10
-	}
-	for ; j >= 0; j-- {
-		buf.tmp[i+j] = pad
-	}
-}
-
 // someDigits formats a zero-prefixed variable-width integer at buf.tmp[i].
 func (buf *buffer) someDigits(i, d int) int {
 	// Print into the top, then copy down. We know there's space for at least
